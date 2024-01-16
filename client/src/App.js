220d02b11e6ff5b665d1143d70675e671c389635
napo <napoleone.cyril@gmail.com>
2024-01-16 10:33:12 +0100

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap'
-import {useState, useEffect} from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
 
+// Clés d'authentification Spotify
 const CLIENT_ID = "2754ffafe92e47b9bd6d17bec67e45b2";
 const CLIENT_SECRET = "d6bf9ff451ec4ca0bb2b1e662c41276e";
 
 function App() {
+  // State pour la valeur de recherche, le token d'accès Spotify, et les albums
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
-  const [albums, setAlbums] = useState([])
+  const [albums, setAlbums] = useState([]);
 
-  useEffect(()=>{
-    //API ACCESS TOKEN
-    var authParameters ={
-      method:'POST',
-      headers:{
-        'Content-Type': 'application/x-www-form-urlencoded'
+  useEffect(() => {
+    // Obtention du token d'accès lors du chargement initial de la page
+    var authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: 'grant_type=client_credentials&client_id='+ CLIENT_ID+'&client_secret='+CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result=>result.json())
-      .then(data=>setAccessToken(data.access_token))
-  }, [])
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
 
-  async function search(){
+  async function search() {
+    // Fonction de recherche appelée lorsqu'un utilisateur effectue une recherche
     console.log("Searching for " + searchInput);
 
-    var searchParameters ={
-      method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
-      }
-    }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q='+searchInput+'&type=artist', searchParameters)
-      .then(response => response.json())
-      .then(data => {return data.artists.items[0].id})
+    // Requête pour obtenir l'ID de l'artiste à partir de la recherche
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+    var artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
 
-    console.log("artist id is "+artistID)
+    console.log("artist id is " + artistID);
 
-    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
-      .then(response => response.json())
-      .then(data => {
+    // Requête pour obtenir les albums de l'artiste
+    var returnedAlbums = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         setAlbums(data.items);
       });
@@ -53,37 +80,43 @@ function App() {
   return (
     <div className="App">
       <Container className="my-5">
+        {/* Barre de recherche avec bouton de recherche */}
         <InputGroup className="mb-3" size="lg">
           <FormControl
             placeholder="Search For Artist"
             type="input"
-            onKeyPress={event=>{
-              if (event.key == "Enter"){
+            onKeyPress={(event) => {
+              if (event.key == "Enter") {
                 search();
               }
             }}
-            onChange={event=> setSearchInput(event.target.value)}
+            onChange={(event) => setSearchInput(event.target.value)}
           />
-          <Button onClick={search}>
-            Search
-          </Button>
+          <Button onClick={search}>Search</Button>
         </InputGroup>
       </Container>
       <Container>
+        {/* Grille pour afficher les albums */}
         <Row className="row-cols-3">
           {albums.map((album, i) => {
-            return(
+            return (
+              // Carte pour afficher chaque album avec un bouton de téléchargement
               <Card className=" my-2">
-                <Card.Img src={album.images[0].url}/>
-                  <Card.Body>
-                    <Card.Title>{album.name}</Card.Title>
-                    <Button href={album.images[0].url} target="_blank" onClick={search} >Download</Button>
-                  </Card.Body>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                  <Button
+                    href={album.images[0].url}
+                    target="_blank"
+                    onClick={search}
+                  >
+                    Download
+                  </Button>
+                </Card.Body>
               </Card>
-            )
+            );
           })}
         </Row>
-        
       </Container>
     </div>
   );
