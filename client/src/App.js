f5aefb0cf1b4f7832dd2a39a6bff3525647c6395
napo <napoleone.cyril@gmail.com>
2024-02-27 10:21:37 +0100

    update jdt and connexion to spotify account button

--- client/src/App.js
+++ client/src/App.js
@@ -51,20 +51,18 @@ function App() {
       .then((data) => setAccessToken(data.access_token));
   }, []);
 
+  // Fonction de connexion Spotify
+  function login() {
+    // Définissez les paramètres pour la requête d'autorisation
+    const authorizeURL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/&scope=user-read-private%20user-read-email&response_type=token&state=123`;
+
+    // Redirigez l'utilisateur vers l'URL d'autorisation
+    window.location = authorizeURL;
+  }
+
   // Fonction de recherche appelée lorsqu'un utilisateur effectue une recherche
   async function search() {
     console.log("Searching for " + searchInput);
-
-    spotifyApi.setAccessToken(accessToken);
-
-    spotifyApi.getMe().then(
-      function (data) {
-        console.log("Some information about the authenticated user", data.body);
-      },
-      function (err) {
-        console.log("Something went wrong!", err);
-      }
-    );
 
     // Requête pour obtenir l'ID de l'artiste à partir de la recherche
     var searchParameters = {
@@ -99,13 +97,17 @@ function App() {
         setAlbums(data.items);
       });
   }
-
   // Rendu de l'interface utilisateur
   return (
     <div className="App-container">
-      <Container className="my-5">
+      <Container className="py-5">
+        {/* Bouton de connexion Spotify */}
+        <Button classname="loginButton" variant="success" onClick={login}>
+          Se connecter à Spotify
+        </Button>
+
         {/* Barre de recherche avec bouton de recherche */}
-        <InputGroup className="mb-3" size="lg">
+        <InputGroup className="mb-3 py-5" size="lg">
           <FormControl
             placeholder="rechercher un artiste"
             type="input"
@@ -119,9 +121,10 @@ function App() {
           <Button onClick={search}>Recherche</Button>
         </InputGroup>
       </Container>
+
+      {/* Grille pour afficher les albums */}
       <Container>
-        {/* Grille pour afficher les albums */}
-        <Row className="row-cols-3">
+        <Row className="row-cols-4">
           {albums.map((album, i) => {
             return (
               // Carte pour afficher chaque album avec un bouton de téléchargement
@@ -129,11 +132,7 @@ function App() {
                 <Card.Img src={album.images[0].url} />
                 <Card.Body>
                   <Card.Title>{album.name}</Card.Title>
-                  <Button
-                    href={album.images[0].url}
-                    target="_blank"
-                    onClick={search}
-                  >
+                  <Button href={album.images[0].url} target="_blank">
                     Télécharger
                   </Button>
                 </Card.Body>
