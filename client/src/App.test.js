28bc3f8973a4cce9991041c947bed3aeba198b86
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-10-09 08:40:33 +0200

    test: add very basic tests for npm test
    will elaborate

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Spotify Covers title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Spotify Covers/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders search input', () => {
+  render(<App />);
+  const searchInput = screen.getByPlaceholderText(/Research an artist/i);
+  expect(searchInput).toBeInTheDocument();
+});
+
+
+test('renders GitHub link in footer', () => {
+  render(<App />);
+  const githubLink = screen.getByText(/Contribute on GitHub/i);
+  expect(githubLink).toBeInTheDocument();
+  expect(githubLink).toHaveAttribute('href', 'https://github.com/cyrilnapo/spotifycovers');
 });
